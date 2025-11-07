6d3f4b82df2dfb4072e68dc059b6f52a3005fb98
Yasmim <santosyasmimcavalcante18@gmail.com>
2025-11-07 19:22:01 -0300

    conexão estabelecida com sucesso✔

---
+++ back-end/conexao.py
@@ -0,0 +1,23 @@
+import psycopg2 as pg
+from dotenv import load_dotenv
+import os 
+
+load_dotenv()
+
+def conector():
+    try:
+        conexao = pg.connect(
+            database=os.getenv("DB_NAME"),
+            user=os.getenv("DB_USER"),
+            password=os.getenv("DB_PASSWORD"),
+            host=os.getenv("DB_HOST"),
+            port=os.getenv("DB_PORT")
+        )
+        cursor = conexao.cursor()
+        
+        return conexao, cursor
+    except Exception as erro:
+        print(f"Error de conexao: {erro}")
+        return None , None
+    
+conector()
